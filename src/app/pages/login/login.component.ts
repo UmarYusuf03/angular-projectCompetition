92864ef03499fe9f7fd7b3ff237c5f586a6111ed
UmarYusuf03 <umaryusuf0307@gmail.com>
2025-12-10 20:20:28 +0530

    created login and register functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  showPassword: boolean = false;
+  loginObj: any = {
+    email: '',
+    password: '',
+  };
 
+  http = inject(HttpClient);
+
+  onLogin() {
+    this.http
+      .post(
+        'https://api.freeprojectapi.com/api/ProjectCompetition/login',
+        this.loginObj
+      )
+      .subscribe((res: any) => {
+        alert('Student logged in.');
+      });
+  }
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  showPassword: boolean = false;
 
+  registerObj: any = {
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    collegeName: '',
+    role: 'Student',
+  };
+
+  http = inject(HttpClient);
+
+  onRegister() {
+    this.http
+      .post(
+        'https://api.freeprojectapi.com/api/ProjectCompetition/register',
+        this.registerObj
+      )
+      .subscribe((res: any) => {
+        alert('Student registration successfull!');
+      });
+  }
 }

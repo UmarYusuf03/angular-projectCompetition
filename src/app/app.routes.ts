3ed4032d1b3980eb765a1b38b296aab650e8457d
UmarYusuf03 <umaryusuf0307@gmail.com>
2025-12-10 16:57:09 +0530

    designed ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { SubmitProjectComponent } from './pages/submit-project/submit-project.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +16,29 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'students',
+    component: StudentsComponent,
+  },
+  {
+    path: 'submitProject',
+    component: SubmitProjectComponent,
+  },
+  {
+    path: 'all-projects',
+    component: ProjectsComponent,
   },
 ];
